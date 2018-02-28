5937cb2b63907c702e078666d769d14e661c7f81
Adrian Lodder <lodder.adrian@gmail.com>
2018-02-27 20:40:06 -0800

    Added comments for sort algorithms

---
+++ ConsoleApp1/BitonicSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    /**
+     * https://www.cs.cmu.edu/~scandal/nesl/alg-sequence.htm
+     * Here is Batcher's bitonic sort. It has total work O(n log^2 n) and depth O(log^2 n).
+     * 
+     * function bitonic_sort(a) =
+     * if (#a == 1) then a
+     * else
+     * let
+     * bot = subseq(a,0,#a/2);
+     * top = subseq(a,#a/2,#a);
+     * mins = {min(bot,top):bot;top};
+     * maxs = {max(bot,top):bot;top};
+     * in flatten({bitonic_sort(x) : x in [mins,maxs]});
+     * 
+     * function batcher_sort(a) =
+     * if (#a == 1) then a
+     * else
+     * let b = {batcher_sort(x) : x in bottop(a)};
+     * in bitonic_sort(b[0]++reverse(b[1]));
+     * 
+     * batcher_sort([8, 14, -8, -9, 5, -9, 17, 19]);
+     */
+    class BitonicSort
+    {
+    }
+}
---
+++ ConsoleApp1/ParallelQuicksort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    /**
+     * https://www.cs.cmu.edu/~scandal/nesl/alg-sequence.htm
+     * This is a parallel version of Quicksort. It has expected work of O(n log n) and an expected depth of O(log n).
+     * 
+     * function quicksort(a) =
+     * if (#a < 2) then a
+     * else
+     * let pivot   = a[#a/2];
+     * lesser  = {e in a| e < pivot};
+     * equal   = {e in a| e == pivot};
+     * greater = {e in a| e > pivot};
+     * result  = {quicksort(v): v in [lesser,greater]};
+     * in result[0] ++ equal ++ result[1];
+     * 
+     * quicksort([8, 14, -8, -9, 5, -9, -3, 0, 17, 19]);
+     */
+    class ParallelQuicksort
+    {
+    }
+}
