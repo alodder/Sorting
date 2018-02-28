9722c14fbb135ec7d51eff8f3295c9f137c810a6
Adrian Lodder <lodder.adrian@gmail.com>
2018-02-27 20:55:19 -0800

    hmm

--- ConsoleApp1/InsertionSort.cs
+++ ConsoleApp1/InsertionSort.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace ConsoleApp1
 {
+
     class InsertionSort
     {
+        /**
+         * Take an array of int
+         * Marking the beginning as sorted, then iterate through the  an array, 
+         * 
+         */
+        public InsertionSort()
+        {
+
+        }
     }
 }
